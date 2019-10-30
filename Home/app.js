ea5fef112863845a7f7e53f252460471db1aade6
Caleb Pendergraft <cackmed@gmail.com>
2019-10-30 09:59:48 -0700

    wrote out app.js file I think

---
+++ Home/app.js
@@ -0,0 +1,60 @@
+import Component from '../Component.js';
+import Header from './header.js';
+import ImageList from './image-list.js';
+import FilterImages from './filter-images.js';
+import images from '../data/images.js';
+
+class App extends Component {
+    
+    onRender(dom){
+        const headerDom = Header.renderDom();
+        dom.prepend(headerDom);
+
+        const props = {
+            images: images
+        };
+
+        const imageList = new ImageList(props);
+        const imageListDom = imageList.renderDom();
+
+        const listSection = dom.querySelector('.list-section');
+        listSection.appendChild(imageListDOM);
+
+        const filterImages = new FilterImages({
+            images: images,
+            onFilter: (imageHorns) => {
+                let filteredImages;
+                if (!imageHorns) {
+                    filteredImages = images;
+                }
+                else {
+                    filteredImages = images.filter(images => {
+                        return images.horns === imageHorns;
+                    });
+                }
+                const updateProps = { images: filteredImages };
+                imageList.updateProps(updateProps);
+            }
+        });
+        const filterImagesDom = filterImages.renderDom();
+
+        const optionsSection = dom.querySelector('.options-section');
+        optionsSection.appendChild(filterImagesDom);
+    }
+    renderHTML() {
+        return /*html*/` 
+        <div>
+        
+                <main>
+                    <section class="menu-section">
+                        
+                    </section>
+                    <div class="grid">
+                        
+                    </section>
+                </main>
+            </div>
+        `;
+
+    }
+}
