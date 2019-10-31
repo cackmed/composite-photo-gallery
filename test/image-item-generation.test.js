e8486a664be27b3b292f82883ce01d1f8774d23c
Caleb Pendergraft <cackmed@gmail.com>
2019-10-30 19:44:00 -0700

    got test to work

--- test/image-item-generation.test.js
+++ test/image-item-generation.test.js
@@ -1,27 +1,29 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
 import ImageItem from '../Home/image-item.js';
+
 const test = QUnit.test;
 
-const imageItem = new ImageItem;
 test('time to test a function', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-    const images = { url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+    const image = { 
+        url: 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
         title: 'UniWhal',
         description: 'A unicorn and a narwhal nuzzling their horns',
         keyword: 'narwhal',
         horns: 1 };
- 
+    const imageItem = new ImageItem({ image });
+
     const expected = `<div class="image-container">
-    <img src="${images.url}" alt="${images.title} image">
+    <img src="${image.url}" alt="${image.title} image">
     <h3>UniWhal</h3>
-    <p>Number of Horns:${images.horns}</p>
+    <p>Number of Horns:${image.horns}</p>
     </div>`;         
     //Act 
     // Call the function you're testing and set the result to a const
 
     //Assert
     // Make assertions about what is expected valid result
-    assert.htmlEqual(imageItem.renderHTML(images), expected);
+    assert.htmlEqual(imageItem.renderHTML(), expected);
 });
